6c029984cbe9f8804f3dea3343f6581647815db9
Muhammad Syahmi Bin Ahmad Kamal <syahmi.ak@okwave.global>
2021-12-22 15:25:56 +0800

    [FEAT] app.routing: New component routing added

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -7,13 +7,15 @@ import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
 import { LandingComponent } from './landing/landing.component';
 import { CoverComponent } from './cover/cover.component';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes =[
     { path: 'home',             component: HomeComponent },
     { path: 'user-profile',     component: ProfileComponent },
     { path: 'landing',          component: LandingComponent },
-    { path: 'cover',             component: CoverComponent },
-    { path: '', redirectTo: 'home', pathMatch: 'full' }
+    { path: 'about',            component: AboutComponent },
+    { path: '',             component: CoverComponent },
+    // { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
 
 @NgModule({
