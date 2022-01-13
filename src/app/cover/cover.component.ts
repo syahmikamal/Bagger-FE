d9cd6f66f34b76f38215b9e0d0ea9f7152800ee1
Muhammad Syahmi Bin Ahmad Kamal <syahmi.ak@okwave.global>
2022-01-13 17:33:57 +0800

    [FIX] cover.component: Spinner loading

--- src/app/cover/cover.component.ts
+++ src/app/cover/cover.component.ts
@@ -1,5 +1,7 @@
 import { Router, Route } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+
 
 @Component({
   selector: 'app-cover',
@@ -9,10 +11,26 @@ import { Component, OnInit } from '@angular/core';
 export class CoverComponent implements OnInit {
 
   constructor(
-    private router: Router
-  ) { }
+    private router: Router,
+    private spinner: NgxSpinnerService
+  ) { 
+    this.spinner.show(undefined,
+      {
+        // type: 'ball-scale-ripple-multiple',
+        // type: 'fire',
+        type: 'square-jelly-box',
+        size: 'medium',
+        bdColor: 'rgba(26,28,35,255)',
+        color: '#11cdef',
+        fullScreen: true
+      });
+  }
 
   ngOnInit(): void {
+    setTimeout(() => {
+      /** spinner ends after 5 seconds */
+      this.spinner.hide();
+    }, 3500);
   }
 
 }
